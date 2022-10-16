4ff22b94c6ee1d3ee35edf9919aa1f8083f536ba
P.J. Henning <pjhenning@fastmail.com>
2022-10-15 23:35:57 -0700

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,4 +1,9 @@
 let
-  pkgs = import <nixpkgs> { crossSystem = { config = "aarch64-unknown-linux-gnu"; }; };
+  pkgs = import <nixpkgs> { 
+    crossSystem = { config = "aarch64-unknown-linux-gnu"; };
+    allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+      "mongodb-4_4"
+    ];
+  };
 in
 pkgs.mongodb-4_4
