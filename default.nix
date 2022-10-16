472c3ac70801fd8245a14715e47efa065faaed57
P.J. Henning <pjhenning@fastmail.com>
2022-10-15 23:30:49 -0700

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
 let
-  pkgs = import <nixpkgs> {};
+  pkgs = import <nixpkgs> { crossSystem = { config = "aarch64-unknown-linux-gnu"; }; };
 in
-pkgs.pkgsCross.aarch64-unknown-linux-gnu.mongodb_4-4
+pkgs.mongodb_4-4
